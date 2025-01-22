a851bfef9219c8cef0d2e4edc4140f4b038b4ca6
Yves Delay <yves@delay-family.com>
2025-01-22 20:29:13 +0100

    fdfsdf

---
+++ .pio/build/esp32initeeprom/libea4/WebServer/detail/mimetable.cpp.d
@@ -0,0 +1,4 @@
+.pio/build/esp32initeeprom/libea4/WebServer/detail/mimetable.cpp.o: \
+ C:/Users/yvesd/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.cpp \
+ C:/Users/yvesd/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.h \
+ C:/Users/yvesd/.platformio/packages/framework-arduinoespressif32/cores/esp32/pgmspace.h
