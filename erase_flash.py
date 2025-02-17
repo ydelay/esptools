35add29f0bee71b9322146d56ebb2660193477f4
ydelay <149329560+ydelay@users.noreply.github.com>
2025-02-17 15:05:48 +0100

    dsfsdf

--- erase_flash.py
+++
@@ -1,8 +0,0 @@
-
-import os
-import subprocess
-
-def before_upload(source, target, env):
-    env.AutodetectUploadPort()
-    upload_port = env.subst("$UPLOAD_PORT")
-    subprocess.check_call(["esptool.py", "--port", upload_port, "erase_flash"])
